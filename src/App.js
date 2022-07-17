68384a18660e05547dfc512588077adc65029f2b
huseyin <huseyinarslan06@gmail.com>
2022-07-17 17:28:52 +0300

    newuserbutton

--- src/App.js
+++ src/App.js
@@ -16,17 +16,54 @@ function App() {
   const url = "https://randomuser.me/api/";
   // const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
-  const [data, setData] = useState("");
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    age: "",
+    location: "",
+    phone: "",
+    password: "",
+    picture: "",
+  });
+  const [title, setTitle] = useState("");
+  const [values, setValues] = useState("");
   const [loading, setLoading] = useState(false);
-  const [propert, setPropert] = useState("");
-  const [userValue, setUserValue] = useState("");
+  const [gender, setGender] = useState("");
 
   const getUser = async () => {
     try {
-      const { data } = await axios.get(url);
-      setData(data.results[0]);
+      await axios.get(url).then((res) => {
+        console.log(res);
+
+        const personImage = res.data.results[0].picture.medium;
+        const personName =
+          res.data.results[0].name.title +
+          " " +
+          res.data.results[0].name.first +
+          " " +
+          res.data.results[0].name.last;
+        const personEmail = res.data.results[0].email;
+        const personPhone = res.data.results[0].phone;
+        const personLocation =
+          res.data.results[0].location.street.number +
+          " " +
+          res.data.results[0].location.street.name;
+        const personPassword = res.data.results[0].login.password;
+        const personAge = res.data.results[0].dob.age;
+
+        setUserInfo({
+          picture: personImage,
+          name: personName,
+          email: personEmail,
+          age: personAge,
+          location: personLocation,
+          phone: personPhone,
+          password: personPassword,
+        });
+
+        setGender(res.data.results[0].gender);
+      });
       setLoading(true);
-      // console.log(data.results[0]);
     } catch (error) {
       console.log(error);
     }
@@ -34,29 +71,41 @@ function App() {
 
   useEffect(() => {
     getUser();
+    setTitle("name");
+    setValues(userInfo.name);
   }, []);
+
+  const handleChange = (e) => {
+    if (e.target.alt === "user") {
+      setTitle("name");
+      setValues(userInfo.name);
+    } else if (e.target.alt === "mail") {
+      setTitle("email");
+      setValues(userInfo.email);
+    } else if (e.target.alt === "age") {
+      setTitle("age");
+      setValues(userInfo.age);
+    } else if (e.target.alt === "map") {
+      setTitle("location");
+      setValues(userInfo.location);
+    } else if (e.target.alt === "phone") {
+      setTitle("phone");
+      setValues(userInfo.phone);
+    } else if (e.target.alt === "lock") {
+      setTitle("password");
+      setValues(userInfo.password);
+    }
+  };
+
+  const handleClick = () => {
+    getUser();
+    setTitle("name");
+    setValues(userInfo.name);
+  };
 
   if (!loading) {
     return <h1>Loading</h1>;
   }
-
-  const {
-    name: { title, first, last },
-    location: { street },
-    email,
-    dob: { age },
-    picture: { medium },
-    phone,
-    login: { password },
-  } = data;
-
-  const handleChange = (e) => {
-    if (e.target.classList.contains("iconImg")) {
-      setPropert(e.target.alt);
-      const value = e.target.alt;
-      setUserValue(userValue[value]);
-    }
-  };
 
   return (
     <main>
@@ -66,34 +115,70 @@ function App() {
 
       <div className="block">
         <div className="container">
-          <img src={medium} alt="random user" className="user-img" />
-          <p className="user-title">My {propert || "name"} is</p>
-          <p className="user-value">{userValue || userValue.name}</p>
-          <div className="values-list" onMouseOver={handleChange}>
+          <img src={userInfo.picture} alt="random user" className="user-img" />
+          <p className="user-title">My {title} is </p>
+          <p className="user-value">{values}</p>
+          <div className="values-list">
             <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" />
+              <img
+                src={gender === "male" ? manSvg : womanSvg}
+                alt="user"
+                id="iconImg"
+                onMouseOver={handleChange}
+              />
             </button>
+
             <button className="icon" data-label="email">
-              <img src={mailSvg} alt="mail" id="iconImg" />
+              <img
+                src={mailSvg}
+                alt="mail"
+                id="iconImg"
+                onMouseOver={handleChange}
+              />
             </button>
+
             <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
+              <img
+                src={womanAgeSvg}
+                alt="age"
+                id="iconImg"
+                onMouseOver={handleChange}
+              />
             </button>
+
             <button className="icon" data-label="street">
-              <img src={mapSvg} alt="map" id="iconImg" />
+              <img
+                src={mapSvg}
+                alt="map"
+                id="iconImg"
+                onMouseOver={handleChange}
+              />
             </button>
+
             <button className="icon" data-label="phone">
-              <img src={phoneSvg} alt="phone" id="iconImg" />
+              <img
+                src={phoneSvg}
+                alt="phone"
+                id="iconImg"
+                onMouseOver={handleChange}
+              />
             </button>
+
             <button className="icon" data-label="password">
-              <img src={padlockSvg} alt="lock" id="iconImg" />
+              <img
+                src={padlockSvg}
+                alt="lock"
+                id="iconImg"
+                onMouseOver={handleChange}
+              />
             </button>
           </div>
 
           <div className="btn-group">
-            <button className="btn" type="button">
-              new user
+            <button className="btn" type="button" onClick={handleClick}>
+              {loading ? "new user" : "loading"}
             </button>
+
             <button className="btn" type="button">
               add user
             </button>
@@ -114,6 +199,7 @@ function App() {
           </table>
         </div>
       </div>
+
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Footer />
       </div>
